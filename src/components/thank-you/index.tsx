76fc49c8ca70c1446b886358efbb476957804b12
amrks7800 <amr.khaled_a646@fsh.kfs.edu.eg>
2025-02-13 03:19:25 +0200

    Fix: add width and height to the image on thank-you/index.tsx to prevent layout shift Add: favicon and title

--- src/components/thank-you/index.tsx
+++ src/components/thank-you/index.tsx
@@ -4,7 +4,7 @@ import iconThankYou from "/images/icon-thank-you.svg"
 const ThankYou = () => {
   return (
     <div className="thank-you">
-      <img src={iconThankYou} alt="" />
+      <img src={iconThankYou} alt="" width={80} height={80} />
       <h1>Thank you!</h1>
       <p>
         Thanks for confirming your subscription! We hope you have fun using our
